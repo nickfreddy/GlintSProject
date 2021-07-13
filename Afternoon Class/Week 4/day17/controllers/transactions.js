af267b17458aeb590ad1647b0689f21025e0cc8a
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-07-13 15:53:51 +0700

    day17

--- Afternoon Class/Week 4/day17/controllers/transactions.js
+++ Afternoon Class/Week 4/day17/controllers/transactions.js
@@ -17,6 +17,29 @@ class Transactions {
       }
     );
   }
+
+  getDetailTransaction(req, res) {
+    connection.query(
+      `SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON g.id = t.id_good  JOIN customers c ON t.id_customer = c.id JOIN suppliers s ON s.id = g.id_supplier WHERE t.id=${req.params.id}`,
+      function (err, results) {
+        if (err) {
+          return res.status(500).json({
+            message: err.message,
+          });
+        }
+
+        if (results.length === 0) {
+          return res.status(404).json({
+            message: 'Transaction not found',
+          });
+        }
+
+        res.status(200).json({
+          data: results,
+        });
+      }
+    );
+  }
 }
 
 module.exports = new Transactions();
--- Afternoon Class/Week 4/day17/routes/transactions.js
+++ Afternoon Class/Week 4/day17/routes/transactions.js
@@ -1,11 +1,17 @@
 const express = require('express'); // Import express
 
 /* Import controller */
-const { getAllTransactions } = require('../controllers/transactions');
+const {
+  getAllTransactions,
+  getDetailTransaction,
+} = require('../controllers/transactions');
 
 const router = express.Router(); // make express router
 
 /* If user (GET) /, it will go here */
 router.get('/', getAllTransactions);
+
+/* If user (GET) /, it will go here */
+router.get('/:id', getDetailTransaction);
 
 module.exports = router;
