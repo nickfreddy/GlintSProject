3d2e341b7d8d5b466edddf8ebdc899dfb19d1ea5
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-07-14 11:58:48 +0700

    day18

--- Morning Class/Week 4/day17/controllers/transactions.js
+++ Morning Class/Week 4/day17/controllers/transactions.js
@@ -1,22 +1,15 @@
-const { connection } = require('../models');
+const { query } = require('../models');
 
 class Transactions {
-  getAllTransactions(req, res) {
+  async getAllTransactions(req, res) {
     try {
-      connection.query(
-        'SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON t.id_good = g.id JOIN customers c ON c.id = t.id_customer JOIN suppliers s ON s.id = g.id_supplier',
-        function (err, results) {
-          if (err) {
-            return res.status(500).json({
-              message: err.message,
-            });
-          }
-
-          res.status(200).json({
-            data: results,
-          });
-        }
+      const data = await query(
+        'SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON t.id_good = g.id JOIN customers c ON c.id = t.id_customer JOIN suppliers s ON s.id = g.id_supplier ORDER BY t.id'
       );
+
+      res.status(200).json({
+        data,
+      });
     } catch (error) {
       res.status(500).json({
         message: error.message,
@@ -25,27 +18,125 @@ class Transactions {
   }
 
   getDetailTransaction(req, res) {
-    try {
-      connection.query(
-        `SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON t.id_good = g.id JOIN customers c ON c.id = t.id_customer JOIN suppliers s ON s.id = g.id_supplier WHERE t.id=${req.params.id}`,
-        function (err, results) {
-          if (err) {
-            return res.status(500).json({
-              message: err.message,
-            });
-          }
-
-          if (results.length === 0) {
-            return res.status(404).json({
-              message: 'Transaction is not found',
-            });
-          }
-
-          res.status(200).json({
-            data: results,
+    query(
+      `SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON t.id_good = g.id JOIN customers c ON c.id = t.id_customer JOIN suppliers s ON s.id = g.id_supplier WHERE t.id=${req.params.id}`
+    )
+      .then((data) => {
+        if (data.length === 0) {
+          return res.status(404).json({
+            message: 'Transaction not found',
           });
         }
+
+        res.status(200).json({
+          data,
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: error.message,
+        });
+      });
+  }
+
+  async createTransaction(req, res) {
+    try {
+      /* Find good price */
+      const good = await query(
+        `SELECT price FROM goods WHERE id=${req.body.id_good}`
       );
+
+      if (good.length === 0) {
+        return res.status(404).json({
+          message: 'Good not found',
+        });
+      }
+
+      const price = good[0].price;
+      const total = eval(price * req.body.quantity);
+
+      /* Insert transactions */
+      const newTransaction = await query(
+        `INSERT INTO transactions(id_good, id_customer, quantity, total) VALUES (${req.body.id_good}, ${req.body.id_customer}, ${req.body.quantity}, ${total})`
+      );
+
+      /* Get the newTransaction data */
+      const data = await query(
+        `SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON t.id_good = g.id JOIN customers c ON c.id = t.id_customer JOIN suppliers s ON s.id = g.id_supplier WHERE t.id=${newTransaction.insertId}`
+      );
+
+      res.status(201).json({
+        data,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  }
+
+  async updateTransaction(req, res) {
+    try {
+      /* Find the transaction */
+      const transaction = await query(
+        `SELECT * FROM transactions WHERE id=${req.params.id}`
+      );
+
+      if (transaction.length === 0) {
+        return res.status(404).json({
+          message: 'Transaction not found',
+        });
+      }
+
+      /* Find good price */
+      const good = await query(
+        `SELECT price FROM goods WHERE id=${req.body.id_good}`
+      );
+
+      if (good.length === 0) {
+        return res.status(404).json({
+          message: 'Good not found',
+        });
+      }
+
+      const price = good[0].price;
+      const total = eval(price * req.body.quantity);
+
+      /* Insert transactions */
+      await query(
+        `UPDATE transactions SET id_good=${req.body.id_good}, id_customer=${req.body.id_customer}, quantity=${req.body.quantity}, total=${total} WHERE id=${req.params.id}`
+      );
+
+      /* Get the updated transaction data */
+      const data = await query(
+        `SELECT t.id, g.name as goodName, c.name as customer, s.name as supplier, t.time, t.quantity, t.total FROM transactions t JOIN goods g ON t.id_good = g.id JOIN customers c ON c.id = t.id_customer JOIN suppliers s ON s.id = g.id_supplier WHERE t.id=${req.params.id}`
+      );
+
+      res.status(201).json({
+        data,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  }
+
+  async deleteTransaction(req, res) {
+    try {
+      const deletedData = await query(
+        `DELETE FROM transactions WHERE id=${req.params.id}`
+      );
+
+      if (deletedData.affectedRows === 0) {
+        return res.status(404).json({
+          message: 'Transaction not found',
+        });
+      }
+
+      res.status(200).json({
+        message: `Transaction with id ${req.params.id} has been deleted`,
+      });
     } catch (error) {
       res.status(500).json({
         message: error.message,
--- Morning Class/Week 4/day17/routes/transactions.js
+++ Morning Class/Week 4/day17/routes/transactions.js
@@ -4,6 +4,9 @@ const express = require('express'); // import express
 const {
   getAllTransactions,
   getDetailTransaction,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
 } = require('../controllers/transactions');
 
 const router = express.Router(); // make express router
@@ -13,6 +16,15 @@ router.get('/', getAllTransactions);
 
 /* If user (GET) /transactions/:id, go here */
 router.get('/:id', getDetailTransaction);
+
+/* If user (POST) /transactions, go here */
+router.post('/', createTransaction);
+
+/* If user (PUT) /transaction/:id, go here */
+router.put('/:id', updateTransaction);
+
+/* If user (DELETE) /transaction/:id, go here */
+router.delete('/:id', deleteTransaction);
 
 // Export router
 module.exports = router;
