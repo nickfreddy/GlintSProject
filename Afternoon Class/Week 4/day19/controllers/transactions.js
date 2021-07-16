7b8e3debff2d06f96c1559f5d60026ae00cc8d39
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-07-16 16:38:17 +0700

    day20

--- Afternoon Class/Week 4/day19/controllers/transactions.js
+++ Afternoon Class/Week 4/day19/controllers/transactions.js
@@ -1,6 +1,74 @@
-const { transaction, good, customer, supplier } = require('../models');
+const { transaction, good, supplier, customer } = require('../models');
 
-class Transaction {
+class Transactions {
+  // Get all transaction data
+  async getAllTransactions(req, res, next) {
+    try {
+      let data = await transaction.findAll({
+        // find all data of transaction table
+        attributes: { exclude: ['id_good', 'id_customer'] },
+        include: [
+          // Include is join
+          {
+            model: customer,
+          },
+          {
+            model: good,
+            include: [
+              // Include is join
+              { model: supplier },
+            ],
+          },
+        ],
+      });
+
+      // If data is nothing
+      if (data.length === 0) {
+        return next({ statusCode: 404, messages: ['Transactions not found'] });
+      }
+
+      // If success
+      res.status(200).json({ data });
+    } catch (e) {
+      // If error
+      next(e);
+    }
+  }
+
+  async getDetailTransaction(req, res, next) {
+    try {
+      let data = await transaction.findOne({
+        // find all data of transaction table
+        where: { id: req.params.id },
+        attributes: { exclude: ['id_good', 'id_customer'] },
+        include: [
+          // Include is join
+          {
+            model: customer,
+          },
+          {
+            model: good,
+            include: [
+              // Include is join
+              { model: supplier },
+            ],
+          },
+        ],
+      });
+
+      // If data is nothing
+      if (!data) {
+        return next({ statusCode: 404, messages: ['Transaction not found'] });
+      }
+
+      // If success
+      res.status(200).json({ data });
+    } catch (e) {
+      // If error
+      next(e);
+    }
+  }
+
   async createTransaction(req, res, next) {
     try {
       // create transaction
@@ -12,7 +80,6 @@ class Transaction {
           id: newData.id,
         },
         attributes: { exclude: ['id_good', 'id_customer'] },
-        // Include is join
         include: [
           {
             model: customer,
@@ -33,6 +100,69 @@ class Transaction {
       next(error);
     }
   }
+
+  // Update data
+  async updateTransaction(req, res, next) {
+    try {
+      // transaction table update data
+      const updatedData = await transaction.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      // If no data updated
+      if (updatedData[0] === 0) {
+        return next({ statusCode: 404, messages: ['Transaction not found'] });
+      }
+
+      // Find the updated transaction
+      const data = await transaction.findOne({
+        where: {
+          id: req.params.id,
+        },
+        attributes: { exclude: ['id_good', 'id_customer'] },
+        include: [
+          {
+            model: customer,
+          },
+          {
+            model: good,
+            include: [
+              {
+                model: supplier,
+              },
+            ],
+          },
+        ],
+      });
+
+      // If success
+      res.status(201).json({ data });
+    } catch (e) {
+      // If error
+      next(e);
+    }
+  }
+
+  // Delete Data
+  async deleteTransaction(req, res, next) {
+    try {
+      // Delete data
+      let data = await transaction.destroy({ where: { id: req.params.id } });
+
+      // If data deleted is null
+      if (!data) {
+        return next({ statusCode: 404, messages: ['Transaction not found'] });
+      }
+
+      // If success
+      res.status(200).json({ message: 'Success delete transaction' });
+    } catch (e) {
+      // If error
+      next(e);
+    }
+  }
 }
 
-module.exports = new Transaction();
+module.exports = new Transactions();
--- Afternoon Class/Week 4/day19/routes/transactions.js
+++ Afternoon Class/Week 4/day19/routes/transactions.js
@@ -1,15 +1,33 @@
 const express = require('express');
 
-/* Import validator */
+// Import validators
 const {
   createTransactionValidator,
+  updateTransactionValidator,
 } = require('../middlewares/validators/transactions');
 
-/* Import controller */
-const { createTransaction } = require('../controllers/transactions');
+// Import controllers
+const {
+  getAllTransactions,
+  createTransaction,
+  getDetailTransaction,
+  deleteTransaction,
+  updateTransaction,
+} = require('../controllers/transactions');
 
 const router = express.Router();
 
-router.post('/', createTransactionValidator, createTransaction);
+// It will find route that has / first after that it will find is it GET or POST
+router
+  .route('/')
+  .get(getAllTransactions)
+  .post(createTransactionValidator, createTransaction);
+
+// It will find route that has /:id first after that it will find is it GET or PUT or DELETE
+router
+  .route('/:id')
+  .get(getDetailTransaction)
+  .put(updateTransactionValidator, updateTransaction)
+  .delete(deleteTransaction);
 
 module.exports = router;
