e1672c2eb4fd50d6f73681cddc5e215554bd4241
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-07-29 10:31:47 +0700

    day29

--- Morning Class/Week 6/day28/tests/transactions.test.js
+++ Morning Class/Week 6/day28/tests/transactions.test.js
@@ -1,16 +1,37 @@
 const request = require('supertest');
+const faker = require('faker');
+const jwt = require('jsonwebtoken');
 const app = require('../index');
-const { good, customer } = require('../models');
+const { good, customer, user } = require('../models');
 let data = [];
+let userToken = '';
+let adminToken = '';
 
 beforeAll(async () => {
   data = await Promise.all([good.find(), customer.find()]);
+
+  const employee = await user.create({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: 'Aneh123!!',
+  });
+
+  const admin = await user.create({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: 'Aneh123!!',
+    role: 'admin',
+  });
+
+  userToken = jwt.sign({ user: employee._id }, process.env.JWT_SECRET);
+  adminToken = jwt.sign({ user: admin._id }, process.env.JWT_SECRET);
 });
 
 describe('/transactions POST', () => {
   it('Transactions must be created', async () => {
     const response = await request(app)
       .post('/transactions')
+      .set('Authorization', `Bearer ${adminToken}`)
       .send({
         id_good: data[0][Math.floor(Math.random() * data[0].length)]._id,
         id_customer: data[1][Math.floor(Math.random() * data[1].length)]._id,
@@ -24,6 +45,7 @@ describe('/transactions POST', () => {
   it('id_good is not valid', async () => {
     const response = await request(app)
       .post('/transactions')
+      .set('Authorization', `Bearer ${adminToken}`)
       .send({
         id_good: 'asdsadasdsadsa',
         id_customer: data[1][Math.floor(Math.random() * data[1].length)]._id,
@@ -37,6 +59,7 @@ describe('/transactions POST', () => {
   it('id_customer is not valid', async () => {
     const response = await request(app)
       .post('/transactions')
+      .set('Authorization', `Bearer ${adminToken}`)
       .send({
         id_good: data[0][Math.floor(Math.random() * data[0].length)]._id,
         id_customer: 'asdsadasdsadsa',
@@ -48,20 +71,39 @@ describe('/transactions POST', () => {
   });
 
   it('id_good and id_customer is not valid', async () => {
-    const response = await request(app).post('/transactions').send({
-      id_good: 'asdsadasdsadsa',
-      id_customer: 'asdsadasdsadsa',
-      quantity: '10',
-    });
+    const response = await request(app)
+      .post('/transactions')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        id_good: 'asdsadasdsadsa',
+        id_customer: 'asdsadasdsadsa',
+        quantity: '10',
+      });
 
     expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(Object);
+  });
+
+  it('Forbidden access for user', async () => {
+    const response = await request(app)
+      .post('/transactions')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({
+        id_good: data[0][Math.floor(Math.random() * data[0].length)]._id,
+        id_customer: data[1][Math.floor(Math.random() * data[1].length)]._id,
+        quantity: '10',
+      });
+
+    expect(response.statusCode).toEqual(403);
     expect(response.body).toBeInstanceOf(Object);
   });
 });
 
 describe('/transactions GET', () => {
   it('Transactions must extst', async () => {
-    const response = await request(app).get('/transactions');
+    const response = await request(app)
+      .get('/transactions')
+      .set('Authorization', `Bearer ${userToken}`);
 
     expect(response.statusCode).toEqual(200);
     expect(response.body).toBeInstanceOf(Object);
