8384b2b4ee38668cb4d19c8c630821cc773b54cd
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-07-30 10:23:03 +0700

    ready to deploy

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(errorHandler);
 
 /* Running server */
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(5000, () => console.log(`Server running on port 3000!`));
+  app.listen(3000, () => console.log(`Server running on port 3000!`));
 }
 
 module.exports = app;
