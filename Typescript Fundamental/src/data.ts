01e0c4723b6c4a69847d655480fe27aac53db88e
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-09-06 10:32:50 +0700

    add typescript

--- Typescript Fundamental/src/data.ts
+++ Typescript Fundamental/src/data.ts
@@ -9,3 +9,16 @@ const david: Data = {
   name: 'David',
   birthday: new Date('2021-10-10'),
 };
+
+const students: Data[] = [
+  {
+    nik: 1232321312,
+    name: 'Adib',
+  },
+  {
+    nik: 12312321,
+    name: 'Gema',
+  },
+];
+
+console.log(students);
